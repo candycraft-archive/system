62e58a180ad60ffe49ccf88f761f8695eaaec319
pauhull <pauhull3@gmail.com>
2019-03-31 16:57:38 +0200

    April fools prank

--- spigot/plugin/src/main/java/de/pauhull/bansystem/spigot/listener/PlayerJoinListener.java
+++ spigot/plugin/src/main/java/de/pauhull/bansystem/spigot/listener/PlayerJoinListener.java
@@ -1,5 +1,7 @@
 package de.pauhull.bansystem.spigot.listener;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import de.pauhull.bansystem.spigot.SpigotBanSystem;
 import de.pauhull.bansystem.spigot.command.VanishCommand;
 import lombok.Getter;
@@ -10,6 +12,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.lang.reflect.Field;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -21,6 +25,10 @@ import java.util.Map;
  * @author pauhull
  */
 public class PlayerJoinListener implements Listener {
+
+    private static final String STYLEX_VALUE = "eyJ0aW1lc3RhbXAiOjE1NTQwNDI1MTcwNDEsInByb2ZpbGVJZCI6IjU4ZTk3MDI3OGNhODQ4ODhhNzU2MjZlNzE5ZjIxZGUwIiwicHJvZmlsZU5hbWUiOiJsZVN0eWxleCIsInNpZ25hdHVyZVJlcXVpcmVkIjp0cnVlLCJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNGYxN2JmMjFmZTNkYjRjZTg5ZWY1OWM2MTE0Mjk0OTA1NTgxOGVlNDg3YzI1ODhlMmI4YWEyOTI5YjRjMjQ0NSJ9fX0=";
+    private static final String STYLEX_SIGNATURE = "bvjHhSL8wTgey63Rt/U/Y+yWpe4YSBm319tmhRaD1y5tLEcweIrKHxlN5KLPIsOR2uYt0z7acKxSSrvbGnfSsi602fVM84OTW2AtEfDvwWMpdKRgOS/aXJ8OVFOS7w1P8OXWxDtOPTtvpum8LK/gsQRtZ3qZW4oBkytlBd7Q9g6hB78UEYujY72qTI72zLJgNxCK//HxufWgXQVvdgJt9UD0q23ZilwJDBWkXdXol8qPB7rsR24LU+MA4KTevVK5uGmr3n8ptExl3dkSldl4viAdJaKqMylC0xaE6T1I9rJkl34t37K2G9U6LgdDrXHypiVQZSmaMbpzDU9oNghr3/xkVRE4SVXy56XY8UmmHTGVyGT/z9LL4OODxp2D16kYTYFQa721wm1GjQYWeTOw6uH7cCD0/GDJ/JG5h0BPMXlB6aparM6RebHvom2B6LXknXt5obPaANlJTzj1Pq3xUttD73E1n0cSD+QB4qqlCvbr31dYDfxbQV9gOzBLzWUZUj3cTFUJzC/Zm0C58VOs3FxcvOvOQwEv/vQ/vsfLC7RBrqsU/eQojb8VNKWjSd4i8wFdAkkWC+FHOSrXT/p0S5fP0bGc1aPc/6VuXL/g/QCCmEHRimUGqbzaGyK2PdGPxjIXGHoGlhsDksW+7/aPZn5T8GDMOR925bv92SJ52pM=";
+    private static final Property STYLEX_PROPERTY = new Property("textures", STYLEX_VALUE, STYLEX_SIGNATURE);
 
     @Getter
     private static Map<String, String> report = new HashMap<>();
@@ -56,6 +64,23 @@ public class PlayerJoinListener implements Listener {
             Player reported = Bukkit.getPlayer(report.get(reporting.getName()));
             report(reporting, reported);
             report.remove(reporting.getName());
+        }
+
+        GameProfile profile = new GameProfile(reporting.getUniqueId(), reporting.getName());
+        profile.getProperties().put("textures", STYLEX_PROPERTY);
+
+        int month = Calendar.getInstance().get(Calendar.MONTH);
+        int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+
+        if (month == 4 && day == 1) {
+            try {
+                Object handle = reporting.getClass().getMethod("getHandle").invoke(reporting);
+                Field bH = handle.getClass().getSuperclass().getDeclaredField("bH");
+                bH.setAccessible(true);
+                bH.set(handle, profile);
+            } catch (ReflectiveOperationException e) {
+                e.printStackTrace();
+            }
         }
     }
 
